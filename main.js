a20c1b0dc8998a475e28a97846d91ca56c4e7b78
Emma Ilstam <emma.ilstam@gmail.com>
2022-10-16 22:13:12 +0200

    Version 1 - work in progress

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let keySolution = [
     "true", 
     "5.3",
     "6.4",
-    "7.2"
+    "7.2",
+    ["8.1", "8.3"]
 ];
 
 let quizAnswer = [];
@@ -13,6 +14,11 @@ let quizAnswer = [];
 let resultBtn = document.querySelector("#result");
 
 resultBtn.addEventListener("click", () => {
+    let test = document.querySelectorAll("[class='eight']:checked")
+    let arr = [];
+    test.forEach((answer) => {
+        arr.push(answer.value);
+    })
     quizAnswer.push(
     document.querySelector("[name='questionOne']:checked").value,
     document.querySelector("[name='questionTwo']:checked").value,
@@ -21,13 +27,25 @@ resultBtn.addEventListener("click", () => {
     document.querySelector("#questionFive").value,
     document.querySelector("#questionSix").value,
     document.querySelector("#questionSeven").value,
-    document.querySelectorAll("[name='questionEight']:checked").value,
+    arr
+    /* document.querySelectorAll("[name='questionEight']:checked").value,
     document.querySelectorAll("[name='questionNine']:checked").value,
-    document.querySelectorAll("[name='questionTen']:checked").value
+    document.querySelectorAll("[name='questionTen']:checked").value */
     );
     let rightAnswer = quizAnswer.filter((answer, i) => {
-        return (answer === keySolution[i]);
+        return (answer == keySolution[i]);
     })
+
+    
+
     console.log(quizAnswer);
     console.log(rightAnswer);
+    console.log(arr);
 })
+
+
+
+
+/* test.forEach((answer) => {
+    rightAnswer.push()
+}) */
